d965457f7d54358e9ecd79985158f74630747f7c
yasmin191 <farhanayassminn@example.com>
2026-02-06 13:01:09 +0500

    feat: Add TranslateButton to all pages via Root component

---
+++ textbook/src/theme/MDXComponents.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import TranslateButton from '@site/src/components/TranslateButton';
+import PodcastPlayer from '@site/src/components/PodcastPlayer';
+
+export default {
+  ...MDXComponents,
+  TranslateButton,
+  PodcastPlayer,
+};
--- textbook/src/theme/Root.tsx
+++ textbook/src/theme/Root.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import RAGChatbot from '../components/RAGChatbot';
+import React from "react";
+import RAGChatbot from "../components/RAGChatbot";
+import TranslateButton from "../components/TranslateButton";
 
 interface RootProps {
   children: React.ReactNode;
 }
 
-// This component wraps the entire site and adds the chatbot to all pages
+// This component wraps the entire site and adds the chatbot and translate button to all pages
 export default function Root({ children }: RootProps): JSX.Element {
   return (
     <>
       {children}
+      <div
+        style={{
+          position: "fixed",
+          top: "70px",
+          right: "20px",
+          zIndex: 100,
+        }}
+      >
+        <TranslateButton />
+      </div>
       <RAGChatbot />
     </>
   );
